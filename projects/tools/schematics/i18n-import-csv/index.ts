63029eec0afdb08a5b65a826b8897a85a93b60f6
alexcibotari <alexandru.cibotari@gmail.com>
2020-11-14 11:28:51 +0100

    add feature to import and export translations in csv

--- projects/tools/schematics/i18n-import-csv/index.ts
+++ projects/tools/schematics/i18n-import-csv/index.ts
@@ -1,10 +1,7 @@
 import {chain, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
 import {FileConfiguration} from '../config';
 
-const SEPARATOR = ',';
-const NEW_LINE = '\n';
-type Dictionary = { [key: string]: string};
-type LanguageDictionary = {[lang: string]: Dictionary};
+type Dictionary = { [key: string]: string };
 
 export default function i18nImportCsv(): Rule {
   return chain([
@@ -14,32 +11,20 @@ export default function i18nImportCsv(): Rule {
       context.logger.info(`Languages : ${config.languages.join(', ')}`);
 
       context.logger.info(`Construct Key to Path map`);
-      const keyToPath: Map<string, string> = createIdToPathMap(tree, context, config.cwd, config.languages[0]);
+      const keyToPath: Map<string, string> = createIdToPathMap(tree, context, config);
+      const langKeyValue: Map<string, Map<string, string>> = readCSV(tree, context, config);
 
-      const dictionary: Map<string, Dictionary> = new Map<string, Dictionary>();
-      let content = `"Key"`;
-      for (const lang of config.languages) {
-        context.logger.info(`Search for '${lang}' files.`);
-        const i18n: any = readLanguage(tree, config.cwd, lang);
-        dictionary.set(lang, i18n);
-        content += `${SEPARATOR}"${lang}"`;
-      }
-      content += NEW_LINE;
-      const first = dictionary.get(config.languages[0]) || {};
-      for (const key of Object.keys(first)) {
-        content += `"${key}"`;
-        for (const lang of config.languages) {
-          let value = '';
-          const l = dictionary.get(lang);
-          if (l !== undefined && l[key] !== undefined) {
-            value = l[key];
+      langKeyValue.forEach((langValue, langKey) => {
+            langValue.forEach((value, key) => {
+              const path = keyToPath.get(key);
+              if (path) {
+                const dic = readLanguage(tree, path, langKey);
+                dic[key] = value;
+                saveLanguage(tree, dic, `${path}/${langKey}.json`);
+              }
+            });
           }
-          content += `${SEPARATOR}"${value}"`;
-        }
-        content += NEW_LINE;
-      }
-      save(tree, content, `${config.output}/languages.csv`);
-      context.logger.info(`Save in '${config.output}/languages.csv'`);
+      );
       return tree;
     }
   ]);
@@ -60,36 +45,73 @@ function readConfig(tree: Tree): FileConfiguration {
   return defaultConfig;
 }
 
-function readCSV(tree: Tree, config: FileConfiguration): Map<string, Dictionary> {
-  const result: Map<string, Dictionary> = new Map<string, Dictionary>();
+function readCSV(tree: Tree, context: SchematicContext, config: FileConfiguration): Map<string, Map<string, string>> {
+  const result: Map<string, Map<string, string>> = new Map<string, Map<string, string>>();
   const buffer: Buffer | null = tree.read(`${config.output}/languages.csv`);
   if (buffer) {
-    const rows: string[] = buffer.toString().split('\n');
+    const languages: string[] = [];
+    const rows: string[] = buffer.toString().split(/\r?\n/);
+    rows.forEach((row, rowIndex) => {
+          // Header read
+          if (rowIndex === 0) {
+            const columns: string[] = row.split('","');
+            columns.forEach((column, columnIndex) => {
+                  if (columnIndex > 0) {
+                    const lang = removeQuote(column);
+                    languages.push(lang);
+                    result.set(lang, new Map<string, string>());
+                  }
+                }
+            );
+          } else {
+            const columns: string[] = row.split('","');
+            let i18nKey = '';
+            columns.forEach((column, columnIndex) => {
+                  if (columnIndex === 0) {
+                    i18nKey = removeQuote(column);
+                  } else {
+                    const lang = languages[columnIndex - 1];
+                    const langMap = result.get(lang);
+                    if (langMap) {
+                      if (langMap.has(i18nKey)) {
+                        context.logger.warn(`The Key '${i18nKey}' is declared more then one time in the csv file.`);
+                      }
+                      langMap.set(i18nKey, removeQuote(column));
+                    }
+                  }
+                }
+            );
+          }
+        }
+    );
   }
   return result;
 }
 
 function readLanguage(tree: Tree, cwd: string, lang: string): Dictionary {
   let i18n: Dictionary = {};
-  tree.getDir(cwd)
-  .visit((filePath) => {
-    if (filePath.search(`/${lang}.json$`) === -1) {
-      return;
-    }
+  const filePath = `${cwd}/${lang}.json`;
+  if (tree.exists(filePath)) {
     const buffer: Buffer | null = tree.read(filePath);
     if (buffer) {
-      i18n = {
-        ...i18n,
-        ...JSON.parse(buffer.toString())
-      };
+      i18n = JSON.parse(buffer.toString());
     }
-  });
+  }
   return i18n;
 }
 
-function createIdToPathMap(tree: Tree, context: SchematicContext, cwd: string, lang: string): Map<string, string> {
+function saveLanguage(tree: Tree, i18n: any, output: string): void {
+  if (tree.exists(output)) {
+    tree.overwrite(output, JSON.stringify(i18n, null, 2));
+  } else {
+    tree.create(output, JSON.stringify(i18n, null, 2));
+  }
+}
+
+function createIdToPathMap(tree: Tree, context: SchematicContext, config: FileConfiguration): Map<string, string> {
   const keyToPath = new Map<string, string>();
-  tree.getDir(cwd)
+  const lang = config.languages[0];
+  tree.getDir(config.cwd)
   .visit((filePath) => {
     if (filePath.search(`/${lang}.json$`) === -1) {
       return;
@@ -110,10 +132,13 @@ function createIdToPathMap(tree: Tree, context: SchematicContext, cwd: string, l
   return keyToPath;
 }
 
-function save(tree: Tree, i18n: string, output: string): void {
-  if (tree.exists(output)) {
-    tree.overwrite(output, i18n);
-  } else {
-    tree.create(output, i18n);
+function removeQuote(text: string): string {
+  let result = text;
+  if (result.charAt(0) === '"') {
+    result = result.substring(1);
   }
+  if (result.charAt(result.length - 1) === '"') {
+    result = result.slice(0, -1);
+  }
+  return result;
 }
