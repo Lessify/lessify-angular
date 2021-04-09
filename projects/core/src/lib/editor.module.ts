e29867fa301515d3b70025456f01a474fbb58c23
Alexandru <alexandru.cibotari@gmail.com>
2021-04-09 21:57:25 +0200

    update listener

--- projects/core/src/lib/editor.module.ts
+++ projects/core/src/lib/editor.module.ts
@@ -1,11 +1,11 @@
-import {NgModule, OnInit, Optional} from '@angular/core';
+import {NgModule, Optional} from '@angular/core';
 import {LessifyTranslationDirective} from './directives/lessify-translation.directive';
 import {LessifyConfigurationDirective} from './directives/lessify-configuration.directive';
 import {TranslateService} from '@ngx-translate/core';
 import {TranslocoService} from '@ngneat/transloco';
 import {DesignEvent} from './models/design.model';
 
-const LESSIFY_WINDOW = 'LessifyEditor';
+const LESSIFY_WINDOW = 'lessify';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ export class LessifyEditorModule {
   ) {
     console.log(`LessifyEditorModule : constructor -> ${this.isInIframe()} - ${window.location}`);
     if (this.isInIframe() && !window[LESSIFY_WINDOW]) {
-      window[LESSIFY_WINDOW] = {};
+      window[LESSIFY_WINDOW] = {editor: true};
       console.log('Start message listener');
       window.addEventListener('message', (event: MessageEvent<DesignEvent>) => {
         console.log(event);
