5e67aa8d724dbee051ea0b62357bc516dc607d11
alexcibotari <alexandru.cibotari@gmail.com>
2022-06-22 21:12:01 +0200

    add new command for environment variable extraction

--- projects/tools/schematics/set-env/index.ts
+++ projects/tools/schematics/set-env/index.ts
@@ -3,10 +3,6 @@ import {Schema} from './schema';
 
 export default function setEnv(options: Schema): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    console.log('options : ' + JSON.stringify(options));
-    console.log('process.env : ' + JSON.stringify(process.env));
-    context.logger.info('options : ' + JSON.stringify(options));
-
     const env: { [key: string]: string } = {};
 
     Object.getOwnPropertyNames(process.env)
@@ -15,17 +11,15 @@ export default function setEnv(options: Schema): Rule {
       env[value] = process.env[value]!;
     });
 
-    console.log('env : ' + JSON.stringify(env));
-
     const content = `export const env = ${JSON.stringify(env, null, 2)};`;
 
-    console.log('content : ' + content);
+    context.logger.info(content);
+
     if (tree.exists(options.path)) {
       tree.overwrite(options.path, content);
     } else {
       tree.create(options.path, content);
     }
-
     return tree;
   };
 }
